87813aab7d2f02e811b2109cd7ff85766f4bd7f8
dougcampbell66 <64913709+dougcampbell66@users.noreply.github.com>
2020-08-10 20:48:44 -0400

    Updating User Controller

--- BugTracker23/Controllers/UsersController.cs
+++ BugTracker23/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BugTracker23.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,17 @@ namespace BugTracker23.Controllers
 {
     public class UsersController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Users
         public ActionResult Index()
         {
-            return View();
+            
+            return View(db.Users.ToList());
+        }
+        public ActionResult ManageUserRole(string id)
+        {
+            ViewBag.RoleName = new SelectList(db.Roles, "Name", "Name");
+            return View(db.Users.Find(id));
         }
     }
 }
